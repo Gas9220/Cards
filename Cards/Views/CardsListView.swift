e2cc4367c5b7a3f28b586dc4dc09358b528bc9b9
Gaspare <Gas9220@users.noreply.github.com>
2023-12-11 16:06:35 +0100

    refactoring: project refactoring

--- Cards/Views/CardsListView.swift
+++ Cards/Views/CardsListView.swift
@@ -1,5 +1,5 @@
 //
-//  CardListView.swift
+//  CardsListView.swift
 //  Cards
 //
 //  Created by Gaspare Monte on 11/12/23.
@@ -7,11 +7,18 @@
 
 import SwiftUI
 
-struct CardListView: View {
+struct CardsListView: View {
     @State private var isPresented = false
 
     var body: some View {
-        ScrollView {
+        list
+            .fullScreenCover(isPresented: $isPresented) {
+                SingleCardView()
+            }
+    }
+
+    var list: some View {
+        ScrollView(showsIndicators: false) {
             VStack {
                 ForEach(0..<10) { _ in
                     CardThumbnail()
@@ -21,13 +28,10 @@ struct CardListView: View {
                 }
             }
         }
-        .scrollIndicators(.hidden)
-        .fullScreenCover(isPresented: $isPresented) {
-            SingleCardView()
-        }
     }
 }
 
+
 #Preview {
-    CardListView()
+    CardsListView()
 }
