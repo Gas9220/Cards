336154d1da7320d8ca22f243e08276a747a84728
Gaspare <Gas9220@users.noreply.github.com>
2023-12-12 11:31:50 +0100

    feat: create CardToolbar view modifier

---
+++ Cards/CardToolbar.swift
@@ -0,0 +1,33 @@
+//
+//  CardToolbar.swift
+//  Cards
+//
+//  Created by Gaspare Monte on 12/12/23.
+//
+
+import SwiftUI
+
+struct CardToolbar: ViewModifier {
+    @Environment(\.dismiss) var dismiss
+    @Binding var currentModal: ToolbarSelection?
+    
+    func body(content: Content) -> some View {
+        content
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Done") {
+                        dismiss()
+                    }
+                }
+                ToolbarItem(placement: .bottomBar) {
+                    BottomToolbar(modal: $currentModal)
+                }
+            }
+            .sheet(item: $currentModal) { item in
+                switch item {
+                default:
+                    Text(String(describing: item))
+                }
+            }
+    }
+}
--- Cards/Views/Single Card Views/SingleCardView.swift
+++ Cards/Views/Single Card Views/SingleCardView.swift
@@ -9,15 +9,15 @@ import SwiftUI
 
 struct SingleCardView: View {
     @Environment(\.dismiss) var dismiss
-    
+
     @State private var currentModal: ToolbarSelection?
-    
+
     var content: some View {
         ZStack {
             Group {
                 Capsule()
                     .foregroundColor(.yellow)
-                
+
                 Text("Resize Me!")
                     .fontWeight(.bold)
                     .font(.system(size: 500))
@@ -25,32 +25,17 @@ struct SingleCardView: View {
                     .lineLimit(1)
             }
             .resizableView()
-            
+
             Circle()
                 .resizableView()
                 .offset(CGSize(width: 50, height: 200))
         }
     }
-    
+
     var body: some View {
         NavigationStack {
             content
-                .toolbar {
-                    ToolbarItem(placement: .topBarTrailing) {
-                        Button("Done") {
-                            dismiss()
-                        }
-                    }
-                    ToolbarItem(placement: .bottomBar) {
-                        BottomToolbar(modal: $currentModal)
-                    }
-                }
-                .sheet(item: $currentModal) { item in
-                    switch item {
-                    default:
-                        Text(String(describing: item))
-                    }
-                }
+                .modifier(CardToolbar(currentModal: $currentModal))
         }
     }
 }
