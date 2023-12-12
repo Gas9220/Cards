70066de0e2462742a80beabd59425c2635ac78a8
Gaspare <Gas9220@users.noreply.github.com>
2023-12-12 10:26:07 +0100

    feat: add RotationGesture to ResizableView

--- Cards/Views/ResizableView.swift
+++ Cards/Views/ResizableView.swift
@@ -13,6 +13,7 @@ struct ResizableView: View {
 
     @State private var transform = Transform()
     @State private var previousOffset: CGSize = .zero
+    @State private var previousRotation: Angle = .zero
 
     var dragGesture: some Gesture {
         DragGesture()
@@ -24,12 +25,25 @@ struct ResizableView: View {
             }
     }
 
+    var rotationGesture: some Gesture {
+        RotationGesture()
+            .onChanged { rotation in
+                transform.rotation += rotation - previousRotation
+                previousRotation = rotation
+            }
+            .onEnded { _ in
+                previousRotation = .zero
+            }
+    }
+
     var body: some View {
         content
             .frame(width: transform.size.width, height: transform.size.height)
             .foregroundStyle(color)
+            .rotationEffect(transform.rotation)
             .offset(transform.offset)
             .gesture(dragGesture)
+            .gesture(rotationGesture)
     }
 }
 
