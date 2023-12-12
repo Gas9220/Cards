5cc8f698fba66ac55f3e8136fe75ec2faf44a6ac
Gaspare <Gas9220@users.noreply.github.com>
2023-12-12 10:15:37 +0100

    feat: create Transform model and ResizableView

---
+++ Cards/Model/Transform.swift
@@ -0,0 +1,14 @@
+//
+//  Transform.swift
+//  Cards
+//
+//  Created by Gaspare Monte on 12/12/23.
+//
+
+import SwiftUI
+
+struct Transform {
+    var size = CGSize(width: 250, height: 180)
+    var rotation: Angle = .zero
+    var offset: CGSize = .zero
+}
---
+++ Cards/Views/ResizableView.swift
@@ -0,0 +1,40 @@
+//
+//  ResizableView.swift
+//  Cards
+//
+//  Created by Gaspare Monte on 12/12/23.
+//
+
+import SwiftUI
+
+struct ResizableView: View {
+    private let content = RoundedRectangle(cornerRadius: 30.0)
+    private let color = Color.red
+    
+    @State private var transform = Transform()
+    @State private var previousOffset: CGSize = .zero
+    
+    var dragGesture: some Gesture {
+        DragGesture()
+            .onChanged { value in
+                transform.offset = CGSize(
+                    width: value.translation.width + previousOffset.width,
+                    height: value.translation.height + previousOffset.height)
+            }
+            .onEnded { _ in
+                previousOffset = transform.offset
+            }
+    }
+    
+    var body: some View {
+        content
+            .frame(width: transform.size.width, height: transform.size.height)
+            .foregroundStyle(color)
+            .offset(transform.offset)
+            .gesture(dragGesture)
+    }
+}
+
+#Preview {
+    ResizableView()
+}
