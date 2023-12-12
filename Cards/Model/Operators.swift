a5722723069354ab59dc6a260d3522f506b95da5
Gaspare <Gas9220@users.noreply.github.com>
2023-12-12 11:35:53 +0100

    feat: *, *=, / operations overload to work better with CGSize

---
+++ Cards/Model/Operators.swift
@@ -0,0 +1,24 @@
+//
+//  Operators.swift
+//  Cards
+//
+//  Created by Gaspare Monte on 12/12/23.
+//
+
+import SwiftUI
+
+func + (left: CGSize, right: CGSize) -> CGSize {
+    CGSize(width: left.width + right.width, height: left.height + right.height)
+}
+
+func * (left: CGSize, right: CGFloat) -> CGSize {
+  CGSize(width: left.width * right, height: left.height * right)
+}
+
+func *= (left: inout CGSize, right: Double) {
+  left = CGSize(width: left.width * right, height: left.height * right)
+}
+
+func / (left: CGSize, right: CGFloat) -> CGSize {
+  CGSize(width: left.width / right, height: left.height / right)
+}
