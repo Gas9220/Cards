54b17e8840a707f917a98af160a98d6f9a83fdf2
Gaspare <Gas9220@users.noreply.github.com>
2023-12-13 11:23:33 +0100

    feat: add abiliy to transfer photos from library to the app

--- Cards/Views/Single Card Views/Card Modal View/PhotosModal.swift
+++ Cards/Views/Single Card Views/Card Modal View/PhotosModal.swift
@@ -10,12 +10,31 @@ import PhotosUI
 
 struct PhotosModal: View {
     @Binding var card: Card
-
+    
     @State private var selectedItems: [PhotosPickerItem] = []
-
+    
     var body: some View {
         PhotosPicker(selection: $selectedItems, matching: .images) {
             ToolbarButton(modal: .photoModal)
+        }
+        .onChange(of: selectedItems) { _, items in
+            for item in items {
+                item.loadTransferable(type: Data.self) { result in
+                    Task {
+                        switch result {
+                        case .success(let data):
+                            if let data,
+                               let uiImage = UIImage(data: data) {
+                                card.addElement(uiImage: uiImage)
+                            }
+                        case .failure(let failure):
+                            fatalError("Image transfer failed: \(failure)")
+                        }
+                    }
+                }
+            }
+            
+            selectedItems = []
         }
     }
 }
