8da0d59143c65a2050cbc52caeffd9e5b31b0b57
Gaspare <Gas9220@users.noreply.github.com>
2023-12-15 14:00:43 +0100

    feat: conform Angle to Codable

---
+++ Cards/Extensions/AngleExtensions.swift
@@ -0,0 +1,25 @@
+//
+//  AngleExtensions.swift
+//  Cards
+//
+//  Created by Gaspare Monte on 15/12/23.
+//
+
+import SwiftUI
+
+extension Angle: Codable {
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let degrees = try container.decode(Double.self, forKey: .degrees)
+        self.init(degrees: degrees)
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(degrees, forKey: .degrees)
+    }
+
+    enum CodingKeys: CodingKey {
+        case degrees
+    }
+}
