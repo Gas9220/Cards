f73b474f47dc372b9f9f36b8c07bf0aa8c75b597
Gaspare <Gas9220@users.noreply.github.com>
2023-12-15 09:38:08 +0100

    feat: add Triangle shape

--- Cards/Model/Shapes.swift
+++ Cards/Model/Shapes.swift
@@ -13,6 +13,25 @@ struct Shapes: View {
     }
 }
 
-#Preview {
-    Shapes()
+#Preview(traits: .sizeThatFitsLayout) {
+    let currentShape = Triangle()
+
+    return currentShape
+        .aspectRatio(1, contentMode: .fit)
+        .background(Color.yellow)
+}
+
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        let width = rect.width
+        let height = rect.height
+        var path = Path()
+        path.addLines([
+            CGPoint(x: width * 0.13, y: height * 0.2),
+            CGPoint(x: width * 0.87, y: height * 0.47),
+            CGPoint(x: width * 0.4, y: height * 0.93)
+        ])
+        path.closeSubpath()
+        return path
+    }
 }
