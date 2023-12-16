c97be739bd2875ac46ffb719c7d1734fd5285f98
Gaspare <Gas9220@users.noreply.github.com>
2023-12-16 10:37:50 +0100

    feat: add cards load and save from/to json

--- Cards/Model/Card.swift
+++ Cards/Model/Card.swift
@@ -8,14 +8,16 @@
 import SwiftUI
 
 struct Card: Identifiable {
-    let id = UUID()
+    var id = UUID()
     var backgroundColor: Color = .yellow
     var elements: [CardElement] = []
-    
+
     mutating func addElement(uiImage: UIImage) {
         let imageFileName = uiImage.save()
         let element = ImageElement(uiImage: uiImage, imageFilename: imageFileName)
         elements.append(element)
+
+        save()
     }
 
     mutating func addElement(text: TextElement) {
@@ -36,10 +38,12 @@ struct Card: Identifiable {
         if let element = element as? ImageElement {
             UIImage.remove(name: element.imageFilename)
         }
-        
+
         if let index = element.index(in: elements) {
             elements.remove(at: index)
         }
+
+        save()
     }
 
     mutating func update(_ element: CardElement?, frameIndex: Int) {
@@ -52,6 +56,38 @@ struct Card: Identifiable {
     }
 
     func save() {
-        print("Saving data")
+        do {
+            let encoder = JSONEncoder()
+            encoder.outputFormatting = .prettyPrinted
+            let data = try encoder.encode(self)
+            let filename = "\(id).rwcard"
+            let url = URL.documentsDirectory.appendingPathComponent(filename)
+
+            try data.write(to: url)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+}
+
+
+extension Card: Codable {
+    enum CodingKeys: CodingKey {
+        case id, backgroundColor, imageElements, textElements
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        let id = try container.decode(String.self, forKey: .id)
+        self.id = UUID(uuidString: id) ?? UUID()
+        elements += try container.decode([ImageElement].self, forKey: .imageElements)
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id.uuidString, forKey: .id)
+        let imageElements: [ImageElement] = elements.compactMap { $0 as? ImageElement }
+        try container.encode(imageElements, forKey: .imageElements)
     }
 }
