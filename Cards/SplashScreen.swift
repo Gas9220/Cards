077d37fb760e28ec578534b64d9fce5d07f35442
Gaspare <Gas9220@users.noreply.github.com>
2023-12-18 16:30:41 +0100

    feat: SplashScreen animation

--- Cards/SplashScreen.swift
+++ Cards/SplashScreen.swift
@@ -9,8 +9,20 @@ import SwiftUI
 
 struct SplashScreen: View {
     var body: some View {
-        card(letter: "C", color: "appColor7")
-            .splashAnimation(finalYposition: 200, delay: 0)
+        ZStack {
+            Color("background")
+                .ignoresSafeArea()
+            card(letter: "S", color: "appColor1")
+                .splashAnimation(finalYposition: 240, delay: 0)
+            card(letter: "D", color: "appColor2")
+                .splashAnimation(finalYposition: 120, delay: 0.2)
+            card(letter: "R", color: "appColor3")
+                .splashAnimation(finalYposition: 0, delay: 0.4)
+            card(letter: "A", color: "appColor6")
+                .splashAnimation(finalYposition: -120, delay: 0.6)
+            card(letter: "C", color: "appColor7")
+                .splashAnimation(finalYposition: -240, delay: 0.8)
+        }
     }
 
     func card(letter: String, color: String) -> some View {
@@ -37,9 +49,17 @@ private struct SplashAnimation: ViewModifier {
     let finalYPosition: CGFloat
     let delay: Double
 
+    let animation = Animation.interpolatingSpring(
+        mass: 0.2,
+        stiffness: 80,
+        damping: 5,
+        initialVelocity: 0.0)
+
     func body(content: Content) -> some View {
         content
             .offset(y: animating ? -700 : finalYPosition)
+            .rotationEffect(animating ? .zero : Angle(degrees: Double.random(in: -10...10)))
+            .animation(animation.delay(delay), value: animating)
             .onAppear {
                 animating = false
             }
